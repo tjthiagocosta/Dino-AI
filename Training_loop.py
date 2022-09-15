f34893b54a08dd30bfcc138eb54682d0e7c966db
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2022-09-15 12:29:11 -0300

    pre install

---
+++ Training_loop.py
@@ -0,0 +1,36 @@
+import gym
+import gym_dinorun
+
+from stable_baselines3 import PPO
+
+
+def main():
+    env = gym.make("DinoRun-v0")
+
+    model = PPO.load('Dino_PPO_440000', env=env)
+    print('Agent created!')
+
+    print("Loop started")
+    for i in range(450000, 1000000, 10000):
+        print(i)
+        # Train the agent
+        model.learn(total_timesteps=10000)
+        print("Finished training!")
+        # Save the agent
+        print("Saving model")
+        model.save(f"Dino_PPO_{i}")
+        print('Model saved!')
+        env.reset()
+        print('Loading new model')
+        model = PPO.load(f'Dino_PPO_{i}', env=env)
+
+    env.reset()
+    #   for _ in range(1000):  # Number of "st eps": Make steps large for training.
+    #        # env.render()
+    #        env.step(env.action_space.sample())  # take a random action
+    print("Closing Environment")
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
--- gym-environments/setup.py
+++ gym-environments/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_dinorun',
       version='0.1',
-      install_requires=['gym', 'selenium', 'numpy', 'pillow', 'pyvirtualdisplay', 'matplotlib', 'opencv-python']
+      install_requires=['gym', 'selenium', 'numpy', 'pillow', 'pyvirtualdisplay', 'matplotlib', 'opencv-python', 'stable_baselines3']
       )
